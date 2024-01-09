6b3cfc48ca48aca0fe8c483233cc890df1f81c9d
N0len-sasha <sasaplatosin@gmail.com>
2024-01-09 12:41:33 +0300

    follows return

--- backend/foodgram_api/views.py
+++ backend/foodgram_api/views.py
@@ -158,7 +158,7 @@ class FoodgramUserViewSet(UserViewSet):
             url_path='subscriptions', permission_classes=(IsAuthenticated,))
     def subscribtions(self, request):
         queryset = FoodgramUser.objects.filter(
-            recipe_owner__subscriber=self.request.user
+            recipeauthor__subscriber=self.request.user
         )
         paginator = PageNumberPagination()
         result_page = paginator.paginate_queryset(queryset, request)
